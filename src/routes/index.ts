0b70e14f683d16e03d59541cde145627c4520311
Jeremiah-Patrick <jeremiah@myserviceagent.net>
2024-03-02 21:57:20 +0100

    chores: changes in route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
 import memberRoutes from './member';
+import workerRoutes from './worker';
 
 const router = Router();
 
 // * Collective routes: Default (index.ts) route
 router.use('/member', memberRoutes);
+router.use('/worker', workerRoutes);
 
 export default router;
